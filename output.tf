c4b98d3faa192e98aad38de7a2a92c75573c1750
pdaambrosio <pda.ambrosio@gmail.com>
2023-10-14 07:58:14 -0300

    fix: description

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "amplify_app_id" {
-  value = aws_amplify_app.amplify_app.id
+  value       = aws_amplify_app.amplify_app.id
+  description = "value of amplify app id"
 }
 
 output "amplify_app_url" {
-  value = aws_amplify_domain_association.domain_association.domain_name
+  value       = aws_amplify_domain_association.domain_association.domain_name
+  description = "value of amplify app url"
 }
